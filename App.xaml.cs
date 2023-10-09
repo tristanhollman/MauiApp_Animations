ae1f0fe9b26d6a0de2ddbe461d137f5112b8c319
Tristan Hollman <tristanhollman@users.noreply.github.com>
2023-10-09 14:55:37 +0200

    Initial

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp_Animations;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace MauiApp_Animations;
+
+public partial class MainPage : ContentPage
+{
+    private bool isVisible = true;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnClick(object sender, EventArgs e)
+    {
+        MyButton.IsEnabled = false;
+        if (isVisible)
+        {
+            await Task.WhenAll
+            (
+                MyImage1.FadeTo(0, 1000),
+                MyImage2.FadeTo(0, 1000)
+            );
+            await MyImage3.FadeTo(0, 1000);
+            MyButton.Text = "Show Image";
+        }
+        else
+        {
+            await Task.WhenAll
+            (
+                MyImage1.FadeTo(1, 1000),
+                MyImage2.FadeTo(1, 1000)
+            );
+            await MyImage3.FadeTo(1, 1000);
+            MyButton.Text = "Hide Image";
+        }
+        isVisible = !isVisible;
+        MyButton.IsEnabled = true;
+    }
+}
+
